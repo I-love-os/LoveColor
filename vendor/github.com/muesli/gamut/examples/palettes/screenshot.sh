d5555b15ef890c2b1eefceb54d094558997e901b
yknomeh <yknomeh@protonmail.com>
2018-11-16 14:03:36 +0100

    removing vendor

--- vendor/github.com/muesli/gamut/examples/palettes/screenshot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-go run main.go
-
-for filename in ${GOPATH}/src/github.com/muesli/gamut/examples/palettes/palette_*.html; do
-    palette=`basename -s .html ${filename}`
-    CutyCapt --url=file://${filename} --out="${GOPATH}/src/github.com/muesli/gamut/docs/${palette}.png" --min-width=900 --min-height=48
-done
